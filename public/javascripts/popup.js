e400fb652e83adf21a97e4a3d273f7d6ee319b85
mlinebac <mlinebac@unca.edu>
2017-07-19 18:03:13 -0400

    adjusted position of popup

--- public/javascripts/popup.js
+++ public/javascripts/popup.js
@@ -1,8 +1,7 @@
 /**
  * Created by matt on 7/16/17.
  */
-function myFunction() {
+myFunction = function() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
-
-}
+};
