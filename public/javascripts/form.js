9d0f994c9503f41c27a0af5239d59fcb41973461
mlinebac <mlinebac@unca.edu>
2017-07-28 18:30:42 -0400

    added firebase database

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,4 +1,6 @@
 $(function () {
+
+    //data variable to keep track of data from Firebase
     var data = [];
 
     var ref = new Firebase('https://takeouttonight-a60b0.firebaseio.com/form');
@@ -7,6 +9,7 @@ $(function () {
 
         console.log(snapshot.val() );
 
+        //putting updated data from Firebase into data variable
         data = snapshot.val();
     })
 
@@ -14,6 +17,37 @@ $(function () {
         var $form = $(this);
         console.log("submit to Firebase");
 
-      $form.find("#saveFrorm").prop('disabled', true);
+      //make submit disabled
+      $form.find("#submitForm").prop('disabled', true);
+
+      //get values to send to firebase
+      var nameToSend = $('#name').val();
+        console.log(nameToSend);
+
+      var emailToSend = $('#email').val();
+        console.log(emailToSend);
+
+      var selectToSend = $('#select').val();
+        console.log(selectToSend);
+
+      var radioToSend = $('#radio').val();
+        console.log(radioToSend);
+
+      var messageToSend = $('#message').val();
+        console.log(messageToSend);
+
+        //creating object with values from form
+        var newForm = {
+            "name": nameToSend,
+            "email": emailToSend,
+            "select": selectToSend,
+            "radio": radioToSend,
+            "message": messageToSend
+        };
+        // putting the object into data array
+        data.push(newForm);
+        console.log(data);
+        //sending data to Firebase
+        ref.set(data);
     })
-})
+});
