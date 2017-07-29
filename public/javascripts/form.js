0280c8563ce6e526f66bff1df49d4ee5e4bb7eea
mlinebac <mlinebac@unca.edu>
2017-07-29 18:05:52 -0400

    refactored code in form.js

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,15 +1,25 @@
 
-    var orderForm = document.getElementById('order-form');
-    var sendButton = document.getElementById('send-button');
+    //var orderForm = document.getElementById('order-form');
+    //var sendButton = document.getElementById('send-button');
+    var database = firebase.database();
 
-    function sendForm (formObj){
-        var firebaseRef = firebase.database().ref('orders/');
-        firebaseRef.set({
-            name: formObj.name.value,
-            email: formObj.email.value,
-            select: formObj.select.value,
-            radio: formObj.radio.value,
-            message: formObj.message.value
+    function sendForm (name, email, selection, radio, message ){
+        var userName = document.getElementById("name");
+        var userEmail = document.getElementById("email");
+        var userSelection = document.getElementById("selection");
+        var userRadio = document.getElementById("radio");
+        var userMessage = document.getElementById("message");
+
+
+
+        database.ref().set({
+            name: userName,
+            email: userEmail,
+            select: userSelection,
+            radio: userRadio,
+            message: userMessage
         });
     }
 
+    var submit = document.getElementByTagName('button')[0];
+    submit.onclick = sendForm;
