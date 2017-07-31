b6992296abb2d6b6854b9ff40dbf32acd22f6565
mlinebac <mlinebac@unca.edu>
2017-07-31 13:41:35 -0400

    adding firebase database to web app

--- app.js
+++ app.js
@@ -22,7 +22,25 @@ var config = {
 };
 
 firebase.initializeApp(config);
+var database = firebase.database();
 
+function sendForm (name, email, selection, radio, message ){
+    var userName = document.getElementById("name");
+    var userEmail = document.getElementById("email");
+    var userSelection = document.getElementById("selection");
+    var userRadio = document.getElementById("radio");
+    var userMessage = document.getElementById("message");
+
+
+
+    database.ref().set({
+        name: userName,
+        email: userEmail,
+        select: userSelection,
+        radio: userRadio,
+        message: userMessage
+    });
+}
 
 
 // view engine setup
