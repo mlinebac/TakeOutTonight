31a1cd106b7c404623cbed573bc1a8683720a910
mlinebac <mlinebac@unca.edu>
2017-08-13 18:14:40 -0400

    added form modal

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,30 +1,43 @@
-var firebase = require('firebase');
-var config = {
-    apiKey: "AIzaSyBeBJ2N_S4V8RusqpFNeGHdweOp9O78gFk",
-    authDomain: "takeouttonight-a60b0.firebaseapp.com",
-    databaseURL: "https://takeouttonight-a60b0.firebaseio.com",
-    projectId: "takeouttonight-a60b0",
-    storageBucket: "takeouttonight-a60b0.appspot.com",
-    messagingSenderId: "78398501206"
-};
-
-firebase.initializeApp(config);
-
-var database = firebase.database();
-
-sendForm = function(name, email, selection, radio, message ){
-    var userName = document.getElementById("name");
-    var userEmail = document.getElementById("email");
-    var userSelection = document.getElementById("selection");
-    var userRadio = document.getElementById("radio");
-    var userMessage = document.getElementById("message");
+firebase.initializeApp({
+    serviceAccount: "./node_modules/TakeOutTonight/TakeOutTonight-7d991d3a4376.json",
+    databaseURL: "https://takeouttonight-a60b0.firebaseio.com"
+});
 
 
-    database.ref('users/').set({
-        name: userName,
-        email: userEmail,
-        select: userSelection,
-        radio: userRadio,
-        message: userMessage
+//places order to firebase from form
+function onClick() {
+    var ref = firebase.database().ref('user orders');
+    var userName = $('#userName').val();
+    var userEmail = $('#userEmail').val();
+    var userSelection = $('#userSelection').val() || [];
+    var userDateSelection = $('#userDateSelection').val() || [];
+    var userMessage = $('#userMessage').val();
+
+    var order = {
+        "name": userName,
+        "email": userEmail,
+        "selection": userSelection,
+        "date": userDateSelection,
+        "message": userMessage,
+        "timestamp": firebase.database.ServerValue.TIMESTAMP
+    };
+
+    ref.push({order});
+
+    ref.limitToLast(1).on("child_added", function(childSnapshot) {
+       ref = childSnapshot.val();
+       var total = 0;
+       var getSelection = ref.order.selection;
+
+       for (var i=0; i<getSelection.length; i++){
+           total += parseInt(getSelection[i].slice(1));
+       }
+
+       $(`#item-selected`).html("<b>Dinner(s) Selected: </b>" + ref.order.selection.join(", "));
+       $('#date-selected').html("<b>Date(s) Selected: </b>" + ref.order.date.join(", "));
+       $('#price-selected').html("<b>Your Total is: </b>" + "$" + total);
+
     });
-};
+
+}
+
---
+++ public/javascripts/menu.js
@@ -0,0 +1,76 @@
+
+
+
+
+firebase.initializeApp({
+    serviceAccount: "./node_modules/TakeOutTonight/TakeOutTonight-7d991d3a4376.json",
+    databaseURL: "https://takeouttonight-a60b0.firebaseio.com"
+});
+// gets reference to items from firebase
+var dbRefItem = firebase.database().ref().child('items');
+
+//code for item-1
+var dbRefItemOne = dbRefItem.child('item-1');
+
+dbRefItemOne.on('value', snap => {
+    dbRefItemOne = snap.val();
+    $('#item-1').html(dbRefItemOne.name);
+    $('#item-1-description').html(dbRefItemOne.description);
+    $('#item-1-price').html("$" + dbRefItemOne.price);
+    $('#dinner1').html (function() {
+        var unitPrice = dbRefItemOne.price;
+        var unitName = dbRefItemOne.name;
+        return ("$" + unitPrice + " " + unitName);
+    });
+});
+
+//code for item-2
+var dbRefItemTwo = dbRefItem.child('item-2');
+
+dbRefItemTwo.on('value', snap => {
+    dbRefItemTwo = snap.val();
+    $('#item-2').html(dbRefItemTwo.name);
+    $('#item-2-description').html(dbRefItemTwo.description);
+    $('#item-2-price').html("$" + dbRefItemTwo.price);
+    $('#dinner2').html (function() {
+        var unitPrice = dbRefItemTwo.price;
+        var unitName = dbRefItemTwo.name;
+        return ("$" + unitPrice + " " + unitName);
+    });
+});
+
+//item-3
+var dbRefItemThree = dbRefItem.child('item-3');
+
+dbRefItemThree.on('value', snap => {
+    dbRefItemThree = snap.val();
+    $('#item-3').html(dbRefItemThree.name);
+    $('#item-3-description').html(dbRefItemThree.description);
+    $('#item-3-price').html("$" + dbRefItemThree.price);
+    $('#dinner3').html (function() {
+        var unitPrice = dbRefItemThree.price;
+        var unitName = dbRefItemThree.name;
+        return ("$" + unitPrice + " " + unitName);
+    });
+});
+//item-4
+var dbRefItemFour = dbRefItem.child('item-4');
+
+dbRefItemFour.on('value', snap => {
+    dbRefItemFour = snap.val();
+    $('#item-4').html(dbRefItemFour.name);
+    $('#item-4-description').html(dbRefItemFour.description);
+    $('#item-4-price').html("$" + dbRefItemFour.price);
+    $('#dinner4').html (function() {
+        var unitPrice = dbRefItemFour.price;
+        var unitName = dbRefItemFour.name;
+        return ("$" + unitPrice + " " + unitName);
+    });
+});
+
+
+
+
+
+
+
